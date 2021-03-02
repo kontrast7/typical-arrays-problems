a81b8225eb047838b62b528d0c9d49ac36702ff1
kontrast7 <maks.mashko@gmail.com>
2021-03-02 22:31:35 +0300

    fix: function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 exports.min = function min(array) {
-
-    if (!array || (array.length === 0)) {
-        return 0;
+    if (Array.isArray(array) && array.length) {
+        return Math.min.apply(null, array);
     } else {
-        return Math.max(array);
+        return 0;
     }
-}
+};
 
 exports.max = function max(array) {
-    if (!array || (array.length === 0)) {
-        return 0;
+    if (Array.isArray(array) && array.length) {
+        return Math.max.apply(null, array);
     } else {
-        return Math.min(array);
+        return 0;
     }
-}
+};
 
 exports.avg = function avg(array) {
-    if (!array || (array.length === 0)) {
-        return 0;
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((a, b) => a + b) / array.length;
     } else {
-        var sum = 0;
-        for (var i = 0; i < array.length; i++) {
-            sum = sum + array[i];
-        }
-        return sum / array.length;
+        return 0;
     }
-}
+};
