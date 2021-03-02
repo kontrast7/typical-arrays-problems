c15194e29e02e4aaeb4e97e65229b923ffce970f
kontrast7 <maks.mashko@gmail.com>
2021-03-02 17:59:33 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (!array || (array.length === 0)) {
+        return 0;
+    } else {
+        return Math.max(array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || (array.length === 0)) {
+        return 0;
+    }
+    return Math.min(array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || (array.length === 0)) {
+    return 0;
+}
+var sum = 0;
+for (var i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+}
+return sum / array.length;
 }
