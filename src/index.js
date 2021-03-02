2b7d034d6a375b88f59bc223a78c3dd7452d36dd
kontrast7 <maks.mashko@gmail.com>
2021-03-02 18:13:32 +0300

    fix: check else

--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ exports.min = function min(array) {
 exports.max = function max(array) {
     if (!array || (array.length === 0)) {
         return 0;
+    } else {
+        return Math.min(array);
     }
-    return Math.min(array);
 }
 
 exports.avg = function avg(array) {
-  if (!array || (array.length === 0)) {
-    return 0;
-}
-var sum = 0;
-for (var i = 0; i < array.length; i++) {
-    sum = sum + array[i];
-}
-return sum / array.length;
+    if (!array || (array.length === 0)) {
+        return 0;
+    } else {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / array.length;
+    }
 }
